14e6867e4534885f6ad843dba4cd69998d1e179b
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-29 15:12:59 -0400

    finished links on Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,41 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
-import { FaCarCrash, FaUser, FaInfo} from 'react-icons/fa';
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { FaCarCrash, FaUser, FaInfo, FaUserEdit } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-        <Navbar.Brand href="/">
-          <FaCarCrash /> Autoparts Outlet
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className='ms-auto'>
-            <Nav.Link href="/cart">
-              <FaUser /> Sign In
-            </Nav.Link>
-            <Nav.Link href="/about">
-              <FaInfo /> About
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <FaCarCrash /> Autoparts Outlet
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/register">
+                <Nav.Link>
+                  <FaUserEdit /> Register
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>
+                  <FaInfo /> About
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
